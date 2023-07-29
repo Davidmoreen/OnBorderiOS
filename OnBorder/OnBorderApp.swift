6e56f6325edc9f7ca9780132402894c5da4a6cda
Davidmoreen <davidmoreen@gmail.com>
2023-07-29 01:20:51 -0400

    Initial Commit

---
+++ OnBorder/OnBorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBorderApp.swift
+//  OnBorder
+//
+//  Created by David and Cristina on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct OnBorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
